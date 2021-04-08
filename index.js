9594f016d99a211f14d6463f304283e1e95e58a9
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-04-08 12:08:58 +0600

    server side almost done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
 
 const port = process.env.PORT || 5000
 
@@ -14,13 +15,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const breakfastCollection = client.db("restaurantdb").collection("breakfast");
-    const lunchCollection = client.db("restaurantdb").collection("lunch");
-    const dinnerCollection = client.db("restaurantdb").collection("dinner");
-    
-    console.log('database connected successfully');
-});
+    const productCollection = client.db("restaurantdb").collection("products");
 
+    // create breakfast data 
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        productCollection.insertOne(product)
+            .then(result => {
+                console.log(result);
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    // retrive data from server 
+    app.get('/allProduct', (req, res) => {
+        productCollection.find()
+            .toArray((error, documents) => {
+                res.status(200).send(documents);
+            })
+    })
+
+    // delete data from database
+    app.delete('/delete/:id', (req, res) => {
+        console.log(req.params.id)
+        productCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => {
+                console.log(result)
+                res.send(result.deletedCount > 0);
+            })
+    })
+
+    //get single product by id
+  app.get('/product/:id', (req, res) => {
+    productCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((error, documents) => {
+        res.send(documents[0]);
+      })
+  })
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
